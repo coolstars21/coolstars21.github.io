b55e890d4927cf81debcd4b02550a5b3f9dc58b9
charango <44603170+charango@users.noreply.github.com>
2019-10-01 11:15:13 +0200

    Delete Cool_Stars_21.js

--- Cool_Stars_21_files/Cool_Stars_21.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20190607
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Cool_Stars_21_files/Cool_Stars_21Moz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
